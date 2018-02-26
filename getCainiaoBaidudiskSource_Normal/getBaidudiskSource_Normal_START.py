06f621dffcfb73266f29b297e45f47401edb08b4
jklf5 <yxwgyxxcy@126.com>
2018-02-27 01:07:41 +0800

    获取“笨鸟先飞”公众号提供的百度网盘链接

---
+++ getCainiaoBaidudiskSource_Normal/getBaidudiskSource_Normal_START.py
@@ -0,0 +1,19 @@
+import urllib.request as ulr
+from lxml import etree
+import re
+
+url = "https://mp.weixin.qq.com/s/zAieVTL_svnigDrIVDUWbQ"
+
+fault = "<p>([0-9|a-z|A-Z|\u4e00-\u9fa5].*?)<br  />"   #匹配中文字符，重要  正则在线测试工具：http://tool.chinaz.com/regex/
+data = ulr.urlopen(url).read().decode("utf-8")
+sourceLinkName = re.compile(fault).findall(data)
+# print(sourceLinkName)
+
+data1 = etree.HTML(data)
+name = data1.xpath("//*[@id='js_content']/p/text()")
+# print(name)
+
+for no in range(1,len(name)):
+    print(name[no])
+# for infoNO in range(0,len(sourceLinkName),2):
+#     print(sourceLinkName[infoNO])
