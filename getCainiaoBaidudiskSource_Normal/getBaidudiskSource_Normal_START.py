a11f31d47a21cc8eb911bb49f6ffb2d1e4e70666
Fire-surface <yxwgyxxcy@126.com>
2018-02-27 03:47:35 +0800

    获取“笨鸟先飞”公众号提供的百度网盘链接，加入文件

--- getCainiaoBaidudiskSource_Normal/getBaidudiskSource_Normal_START.py
+++ getCainiaoBaidudiskSource_Normal/getBaidudiskSource_Normal_START.py
@@ -1,6 +1,8 @@
 import urllib.request as ulr
 from lxml import etree
 import re
+import json
+import string
 
 url = "https://mp.weixin.qq.com/s/zAieVTL_svnigDrIVDUWbQ"
 
@@ -10,10 +12,35 @@ sourceLinkName = re.compile(fault).findall(data)
 # print(sourceLinkName)
 
 data1 = etree.HTML(data)
-name = data1.xpath("//*[@id='js_content']/p/text()")
+info = data1.xpath("//*[@id='js_content']/p/text()")
+# print(info)
 
-for no in range(1,len(name)):
-    a = name[no].split("链接:")
-    print(a)
-# for infoNO in range(0,len(sourceLinkName),2):
-#     print(sourceLinkName[infoNO])
+fh = open("D:/allFile/Python_Project/Spider/getCainiaoBaidudiskSource_Normal/data/info.txt","w",encoding="utf-8")
+
+for no in range(1,24):
+    a = info[no].split(' ',4)
+    if len(a) == 4:
+        fh.write(' '.join(a)+'\n')
+    else:
+        fh.write(' '.join(a)+'\n')
+
+surpriseName = data1.xpath("//*[@id='js_content']/p[45]/span[2]/text()")   #单独加入“优质源码”的数据
+surpriseLink = data1.xpath("//*[@id='js_content']/p[45]/text()")
+fh.write(str(surpriseName[0])+'\n')
+fh.write(str(surpriseLink[0])+'\n')
+
+# surprise2 = data1.xpath("//*[@id='js_content']/p[46]/text()")  #单独加入第25个数据
+# print(re.split(':|：|\n| ',surprise2[0]))
+# print(len(surprise2))
+# fh.write(surprise2[0]+'\n')
+# fh.write(surprise2[1]+':'+surprise2[2]+':'+surprise2[3]+' '+surprise2[4]+':'+surprise2[5])
+
+for no in range(26,43):
+    a = re.split(':|：',info[no])
+    fh.write(a[0]+'\n')
+    b = ""
+    for node in range(1,len(a)):
+        b += a[node]+":"
+    fh.write(b+'\n')
+
+fh.close()
