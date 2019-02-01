856eb7d490c8c071ae98eddac06b1208c4ea8f28
jklf5 <691805562@qq.com>
2019-02-02 00:51:33 +0800

    获取到老师的姓名与照片

--- getTutorInfoOf_JSU/getTutorInfoOf_JSU.py
+++ getTutorInfoOf_JSU/getTutorInfoOf_JSU.py
@@ -1,4 +1,35 @@
 import urllib.request
+from lxml import etree
+root = "J:/allFile/Python_Project/Spider/getTutorInfoOf_JSU/Data/IMG/"
 
 url = "http://cs.ujs.edu.cn/info/1052/5807.htm"
+
+tr_list = []
+
+def downloadIMG(url_img,name):
+    urllib.request.urlretrieve(url_img,root+name+".jpg")
+
+html = urllib.request.urlopen(url)
+text = html.read()
+text = text.decode('utf-8','ignore')
+#print(text)
+
+ehtml = etree.HTML(text)
+#print(ehtml)
+
+html_data = ehtml.xpath('//table')
+for table in html_data:
+    tr_list.append(table.xpath('.//tr[1]/td[3]'))
+
+print(tr_list[0][0].text.strip())
+name = tr_list[0][0].text
+
+#下载图片
+html_data_img = ehtml.xpath('//table[1]/tbody/tr[1]/td[1]/img/@src')[0]
+#print(html_data_img)
+
+url_img = "http://cs.ujs.edu.cn" + html_data_img
+
+downloadIMG(url_img,name.strip())
+
 
