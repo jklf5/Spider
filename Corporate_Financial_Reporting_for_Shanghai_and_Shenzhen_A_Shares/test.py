1686fc89e22fe6dfc2bf0752be617e751855217e
jklf5-pc <691805562@qq.com>
2020-01-04 23:48:59 +0800

    爬取A股上市公司财报 Version 2.0 爬取杜邦分析换从新浪股票处

--- Corporate_Financial_Reporting_for_Shanghai_and_Shenzhen_A_Shares/test.py
+++ Corporate_Financial_Reporting_for_Shanghai_and_Shenzhen_A_Shares/test.py
@@ -7,9 +7,17 @@ import random
 from pyquery import PyQuery as pq
 from pathlib import Path
 import threading
+import threadpool
+from multiprocessing import Pool
+from functools import partial
+# from pathos.multiprocessing import ProcessingPool as Pool
 
+mutex = threading.Lock()
+sem = threading.Semaphore(3)
+time_start = time.time()
 class my_thread(threading.Thread):
     def __init__(self, url, each_time, each_url, downloadpath, list_event_name, my_proxies):
+    # def __init__(self, url, each_time, each_url, downloadpath, list_event_name):
         super(my_thread, self).__init__ ()
         self.url = url
         self.each_time = each_time
@@ -18,7 +26,8 @@ class my_thread(threading.Thread):
         self.list_event_name = list_event_name
         self.my_proxies = my_proxies
     def run(self):
-        f(self.url, self.each_time, self.each_url, self.downloadpath, self.list_event_name, self.my_proxies)
+        # f(self.url, self.each_time, self.each_url, self.downloadpath, self.list_event_name, self.my_proxies)
+        f(self.url, self.each_time, self.each_url, self.downloadpath)
 
 my_headers={
     "Host": "quotes.money.163.com",
@@ -31,9 +40,9 @@ def get_proxies():
     proxyHost = "http-pro.abuyun.com"
     proxyPort = "9010"
 
-    # 代理隧道验证信息 HU6667J179X422GP:31887D12C64DB007
-    proxyUser = "HU6667J179X422GP"
-    proxyPass = "31887D12C64DB007"
+    # 代理隧道验证信息 H89329100P6Q2B4P:CA5B80409B6DA596
+    proxyUser = "H89329100P6Q2B4P"
+    proxyPass = "CA5B80409B6DA596"
 
     proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
       "host" : proxyHost,
@@ -47,12 +56,17 @@ def get_proxies():
     }
     return proxies
 
-def f(url, each_time, each_url, downloadpath, list_event_name, my_proxies):
+# def f(url, each_time, each_url, downloadpath, my_proxies):
+def f(url, each_time, each_url, downloadpath):
     # 抓取每个报告期的数据
     # for (each_time, each_url) in zip(list_report_time, list_report_url):
+    # with sem:
+        # if mutex.acquire(1):
+    list_event_name = ['净资产收益率', '总资产收益率', '权益乘数', '销售净利率', '总资产周转率', '净利润', '营业收入', '营业收入', '平均资产总额', '营业收入', '全部成本', '投资收益', '所得税', '其他', '营业成本', '销售费用', '管理费用', '财务费用']
     f = open(downloadpath, 'a+') #将文件打开为追加模式
     url_dbfx = url +"?date=" + str(each_url) # 每个报告期的链接
-    data_each_report = requests.get(url_dbfx, headers=my_headers, proxies=my_proxies, stream=True)
+    # data_each_report = requests.get(url_dbfx, headers=my_headers, proxies=my_proxies, stream=True)
+    data_each_report = requests.get(url_dbfx, headers=my_headers, stream=True)
     html_data_each_report = pq(data_each_report.text)
     items_each_report = html_data_each_report('.dbbg02').items()
     f.write(each_time + '\t' + url_dbfx + "\n") #存每个报告期的具体时间
@@ -64,7 +78,8 @@ def f(url, each_time, each_url, downloadpath, list_event_name, my_proxies):
     # print('\n')
     f.close()
     print(each_time)
-    time.sleep(0.2)
+    # time.sleep(0.2)
+    # mutex.release()
 
 my_proxies = get_proxies()
 
@@ -74,10 +89,10 @@ work_cwd = os.path.abspath('..')
 
 path_reporting = work_cwd + r'/Corporate_Financial_Reporting_for_Shanghai_and_Shenzhen_A_Shares/Financial_reporting'
 
-data_temp = requests.get(url, headers=my_headers, proxies=my_proxies, stream=True)
+# data_temp = requests.get(url, headers=my_headers, proxies=my_proxies, stream=True)
+data_temp = requests.get(url, headers=my_headers, stream=True)
 downloadpath = path_reporting + '/test_dbfx.txt'
 
-list_event_name = ['净资产收益率', '总资产收益率', '权益乘数', '销售净利率', '总资产周转率', '净利润', '营业收入', '营业收入', '平均资产总额', '营业收入', '全部成本', '投资收益', '所得税', '其他', '营业成本', '销售费用', '管理费用', '财务费用']
 list_report_time = list()
 list_report_url = list()
 html_data = pq(data_temp.text)
@@ -87,13 +102,35 @@ for each in items:
     list_report_url.append(each.attr('value'))
 # print(list_report_time)
 
-thread_assemble = []
-for (each_time, each_url) in zip(list_report_time, list_report_url):
-    t = my_thread(url, each_time, each_url, downloadpath, list_event_name, my_proxies)
-    print(t.getName)
-    t.start()
-    # t.join()
-    thread_assemble.append(t)
+# thread_assemble = []
+# for (each_time, each_url) in zip(list_report_time, list_report_url):
+#     t = my_thread(url, each_time, each_url, downloadpath, list_event_name, my_proxies)
+#     # t = my_thread(url, each_time, each_url, downloadpath, list_event_name)
+#     print(t.getName())
+#     t.start()
+#     # t.join()
+#     # thread_assemble.append(t)
 
-for thread in thread_assemble:
-    thread.join()
+# # for thread in thread_assemble:
+#     # print(threading.current_thread().name)
+#     # thread.join()
+# requests_pool_list = []
+pool = threadpool.ThreadPool(3)
+# for (each_time, each_url) in zip(list_report_time, list_report_url):
+#     requests_pool_each = threadpool.makeRequests(f, args_list=[url, each_time, each_url, downloadpath, list_event_name])
+#     requests_pool_list.append(requests_pool_each)
+arguments = []
+for (each_time, each_url) in zip(list_report_time, list_report_url):
+    dict_temp = {'url':url, 'each_time':each_time, 'each_url':each_url, 'downloadpath':downloadpath}
+    arguments_temp = (None, dict_temp)
+    arguments.append(arguments_temp)
+# print(arguments)
+requests_pool = threadpool.makeRequests(f, arguments)
+# map(pool.putRequest, requests_pool_list)
+# for req in requests_pool:
+#     pool.putRequest(req)
+[pool.putRequest(req) for req in requests_pool] 
+pool.wait()
+time_end = time.time()
+print(time_end-time_start)
+
---
+++ Corporate_Financial_Reporting_for_Shanghai_and_Shenzhen_A_Shares/test2.py
@@ -0,0 +1,53 @@
+#coding=utf-8
+import requests
+from pyquery import PyQuery as pq
+
+url = "http://vip.stock.finance.sina.com.cn/corp/go.php/vFD_DupontAnalysis/stockid/002385.html"
+list_report_time = []
+list_1_1 = []
+list_all = []
+data_temp = requests.get(url)
+
+# 网页按gb2312编码，但是python3中只有bytes才能用decode，所以用content获取bytes类型
+html_data = pq(data_temp.content.decode('gb2312'))
+
+item = html_data('.datelist a').items()
+for each in item:
+    list_report_time.append(each.text())
+item1 = html_data('.node.node-row-1 p:last-child').items()
+for each in item1:
+    list_1_1.append(each.text())
+for row in range(6):
+    for column in range(7):
+        item2 = html_data('.node.node-row-'+ str(row+1) + '-' + str(column+1) + ' p:last-child').items()
+        # if(item2)
+        for each in item2:
+            if each.text() == None:
+                break
+            else:
+                # print(each.text())
+                list_all.append(each.text())
+
+# print(list_1_1)
+# print(list_all)
+
+list_event_name = ['净资产收益率', '归属母公司股东的销售净利率', '资产周转率(次)', '权益乘数', '销售净利率', '归属母公司股东的净利润占比',
+'营业总收入', '平均总资产', '平均总资产', '平均归属母公司股东的利益', '经营利润率', '考虑税负因素', '考虑利息负担', '归属母公司股东净利润',
+'净利润', '期末总资产', '期末归属母公司股东的利益', 'EBIT', '净利润', '利润总额', '期初总资产', '期初归属母公司股东的利益', '营业总收入', '利润总额', 'EBIT']
+
+f = open("j://test.txt", 'a+')
+f.write(url + '\n')
+for each in range(len(list_report_time)):
+    f.write(list_report_time[each] + '\n')
+    f.write(list_event_name[0] + ":" + list_1_1[each] + " ")
+    for i in range(24):
+        report_time_length = len(list_report_time)
+        '''
+        网页上爬下来的数据是按某一名称分类的，而写入是按某一报告期分类。
+        杜邦分析每个报告期共25个数据，去掉第一个数据是单独存放的以外，其余共有24种名称的数据。
+        存放的list_all列表是按照名称来存放，也就是存放了24种名称的数据。每种名称的数据共len(list_report_time)个
+        所以得到求每个报告期（each），每一种名称（i）的数据公式为：i*length+each
+        '''
+        f.write(list_event_name[i+1] + ":" + list_all[i*report_time_length+each] + " ")
+    f.write('\n')
+f.close()
