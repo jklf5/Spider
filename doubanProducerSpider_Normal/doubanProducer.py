94c5dc9dc7057f3a9c828eeee0597f0cc2881945
jklf5-pc <691805562@qq.com>
2020-02-18 15:51:21 +0800

    :art: 从豆瓣上获取所有出版商名称

--- doubanProducerSpider_Normal/doubanProducer.py
+++ doubanProducerSpider_Normal/doubanProducer.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 import re
 import urllib.request
-port='<div class="name">(.*?)</div>'
-data=urllib.request.urlopen("https://read.douban.com/provider/all").read()
-data=data.decode("utf-8")
-result=re.compile(port).findall(data)
+port = '<div class="name">(.*?)</div>'
+data = urllib.request.urlopen("https://read.douban.com/provider/all").read()
+data = data.decode("utf-8")
+result = re.compile(port).findall(data)
 print(result)
-fh=open("e:/doubanProducer.txt","w")
-for each in result :
-	fh.writelines(each+"\n")
+fh = open("e:/doubanProducer.txt", "w")
+for each in result:
+    fh.writelines(each+"\n")
 fh.close()
+#
