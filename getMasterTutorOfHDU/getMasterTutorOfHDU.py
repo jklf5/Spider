64627ec742fc056f5513c3f53528e7ef9d3b8a83
jklf5-pc <691805562@qq.com>
2020-02-18 15:52:10 +0800

    :art: 从杭电官网获取杭电计算机学院所有导师的公开信息-未完成

--- getMasterTutorOfHDU/getMasterTutorOfHDU.py
+++ getMasterTutorOfHDU/getMasterTutorOfHDU.py
@@ -4,22 +4,26 @@ from lxml import etree
 for m in range(6):
     url = "http://computer.hdu.edu.cn/1357/list"+str(m)+".htm"
     data = urllib.request.urlopen(url).read().decode("utf-8")
-    #print(data)
+    # print(data)
 
     html = etree.HTML(data)
-    html_data = html.xpath('/html/body/div[1]/div[5]/div[2]/div[2]/div/ul/li/a/@href')
+    html_data = html.xpath(
+        '/html/body/div[1]/div[5]/div[2]/div[2]/div/ul/li/a/@href')
     for i in html_data:
         url_temp = "http://computer.hdu.edu.cn" + i
         print(url_temp)
         data_temp = urllib.request.urlopen(url_temp).read().decode("utf-8")
 
         html_temp = etree.HTML(data_temp)
-        teacher_name = html_temp.xpath('/html/body/div[1]/div[5]/div[2]/div[2]/div/h3/text()')
+        teacher_name = html_temp.xpath(
+            '/html/body/div[1]/div[5]/div[2]/div[2]/div/h3/text()')
         print(teacher_name[0])
-        
-        html_data_temp = html_temp.xpath('//div[@class="wp_articlecontent"]/p/span')
+
+        html_data_temp = html_temp.xpath(
+            '//div[@class="wp_articlecontent"]/p/span')
         strinfo = ""
         for k in html_data_temp:
             if(k != None):
                 strinfo += k.text
         print(strinfo)
+#
