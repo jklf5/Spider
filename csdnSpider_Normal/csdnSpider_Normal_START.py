5e11b12b3cb117808557e73a24e093579411e7d5
jklf5-pc <691805562@qq.com>
2020-02-18 15:49:37 +0800

    :art: 获取CSDN首页新闻

--- csdnSpider_Normal/csdnSpider_Normal_START.py
+++ csdnSpider_Normal/csdnSpider_Normal_START.py
@@ -1,27 +1,29 @@
 import urllib.request
 import re
 
-url="http://www.csdn.net/"
-data=urllib.request.urlopen(url).read()
-data=data.decode("utf-8","ignore")
+url = "http://www.csdn.net/"
+data = urllib.request.urlopen(url).read()
+data = data.decode("utf-8", "ignore")
 
-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
-headers=(agent)
-opener=urllib.request.build_opener()
-opener.addheaders=[headers]
+agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+headers = (agent)
+opener = urllib.request.build_opener()
+opener.addheaders = [headers]
 
-pat='href="(http://blog.csdn.net/.*?)"'
-allURL=re.compile(pat).findall(data)
-for i in range(0,len(allURL)):
-	try:
-		print("第"+str(i)+"条新闻")
-		thisURL=allURL[i]
-		print(thisURL)
-		file="E:/allFile/Python_Project/Spider/csdnSpider_Normal/csdnNews/"+"csdnNews"+str(i)+"(2018.02.04 22.35)"+".html"
-		urllib.request.urlretrieve(thisURL,file)
-		print("------成功------")
-	except urllib.error.URLError as e:
-		if hasattr(e,"code"):
-			print(e.code)
-		if hasattr(e,"reason"):
-			print(e.reason)
+pat = 'href="(http://blog.csdn.net/.*?)"'
+allURL = re.compile(pat).findall(data)
+for i in range(0, len(allURL)):
+    try:
+        print("第"+str(i)+"条新闻")
+        thisURL = allURL[i]
+        print(thisURL)
+        file = "E:/allFile/Python_Project/Spider/csdnSpider_Normal/csdnNews/" + \
+            "csdnNews"+str(i)+"(2018.02.04 22.35)"+".html"
+        urllib.request.urlretrieve(thisURL, file)
+        print("------成功------")
+    except urllib.error.URLError as e:
+        if hasattr(e, "code"):
+            print(e.code)
+        if hasattr(e, "reason"):
+            print(e.reason)
+#
